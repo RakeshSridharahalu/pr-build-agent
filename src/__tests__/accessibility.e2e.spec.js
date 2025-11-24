1f27a5774e5b21dfde25db047c57562dc9d5f983
RakeshSridharahalu <rakesh.sridharahalu2004@gmail.com>
2025-11-24 12:46:39 +0530

    updations

--- src/__tests__/accessibility.e2e.spec.js
+++ src/__tests__/accessibility.e2e.spec.js
@@ -1,8 +1,23 @@
-import { test } from "@playwright/test";
+// import { test } from "@playwright/test";
+// import { injectAxe, checkA11y } from "axe-playwright";
+
+// test("E2E Accessibility Scan", async ({ page }) => {
+//   await page.goto("/");
+//   await injectAxe(page);
+//   await checkA11y(page);
+// });
+
+import { test, expect } from "@playwright/test";
 import { injectAxe, checkA11y } from "axe-playwright";
 
 test("E2E Accessibility Scan", async ({ page }) => {
-  await page.goto("/");
+  await page.goto("http://localhost:5173");
+
   await injectAxe(page);
-  await checkA11y(page);
+
+  // Run axe checks
+  await checkA11y(page, null, {
+    detailedReport: true,
+    detailedReportOptions: { html: true },
+  });
 });
